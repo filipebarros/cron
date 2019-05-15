afed5c7f173f31a71b5b9a1514eb8da2b3178d26
Filipe Barros <afbarros@me.com>
2019-05-15 22:44:15 +0100

    simplify tests

--- spec/cron/parser_spec.rb
+++ spec/cron/parser_spec.rb
@@ -27,187 +27,79 @@ RSpec.describe Cron::Parser do
     end
   end
 
-  describe '#handle' do
+  shared_examples 'regexes' do
     subject(:handler) { described_class.handle(string, type) }
 
-    context 'with minute' do
+    describe 'when string is `*`' do
+      let(:string) { '*' }
+
+      it 'lists all values in range' do
+        expect(handler).to eq((minimum..maximum).to_a)
+      end
+    end
+
+    describe 'when string is `*/3`' do
+      let(:string) { '*/3' }
+
+      it 'lists all values with 3 as the step' do
+        expect(handler).to eq((minimum..maximum).step(3).to_a)
+      end
+    end
+
+    describe 'when string is `2-5`' do
+      let(:string) { '2-5' }
+
+      it 'lists all values from 2 to 5' do
+        expect(handler).to eq([2, 3, 4, 5])
+      end
+    end
+
+    describe 'when string is `1,3,4`' do
+      let(:string) { '1,3,4' }
+
+      it 'lists all values passed to the method' do
+        expect(handler).to eq([1, 3, 4])
+      end
+    end
+  end
+
+  describe 'minute' do
+    it_behaves_like 'regexes' do
       let(:type) { :minute }
-
-      describe 'when string is `*`' do
-        let(:string) { '*' }
-
-        it 'lists all minutes from 0 to 59' do
-          expect(handler).to eq((0..59).to_a)
-        end
-      end
-
-      describe 'when string is `*/10`' do
-        let(:string) { '*/10' }
-
-        it 'lists all minutes with 10 as the step' do
-          expect(handler).to eq([0, 10, 20, 30, 40, 50])
-        end
-      end
-
-      describe 'when string is `7-10`' do
-        let(:string) { '7-10' }
-
-        it 'lists all minutes from 7 to 10' do
-          expect(handler).to eq([7, 8, 9, 10])
-        end
-      end
-
-      describe 'when string is `10,15,16`' do
-        let(:string) { '10,15,16' }
-
-        it 'lists all minutes passed to the method' do
-          expect(handler).to eq([10, 15, 16])
-        end
-      end
+      let(:minimum) { 0 }
+      let(:maximum) { 59 }
     end
+  end
 
-    context 'with hour' do
+  describe 'hour' do
+    it_behaves_like 'regexes' do
       let(:type) { :hour }
-
-      describe 'when string is `*`' do
-        let(:string) { '*' }
-
-        it 'lists all hours from 0 to 23' do
-          expect(handler).to eq((0..23).to_a)
-        end
-      end
-
-      describe 'when string is `*/10`' do
-        let(:string) { '*/10' }
-
-        it 'lists all hours with 10 as the step' do
-          expect(handler).to eq([0, 10, 20])
-        end
-      end
-
-      describe 'when string is `7-10`' do
-        let(:string) { '7-10' }
-
-        it 'lists all hours from 7 to 10' do
-          expect(handler).to eq([7, 8, 9, 10])
-        end
-      end
-
-      describe 'when string is `10,15,16`' do
-        let(:string) { '10,15,16' }
-
-        it 'lists all hours passed to the method' do
-          expect(handler).to eq([10, 15, 16])
-        end
-      end
+      let(:minimum) { 0 }
+      let(:maximum) { 23 }
     end
+  end
 
-    context 'with day of the month' do
+  context 'with day of the month' do
+    it_behaves_like 'regexes' do
       let(:type) { :day_of_the_month }
-
-      describe 'when string is `*`' do
-        let(:string) { '*' }
-
-        it 'lists all days of the month from 1 to 31' do
-          expect(handler).to eq((1..31).to_a)
-        end
-      end
-
-      describe 'when string is `*/10`' do
-        let(:string) { '*/10' }
-
-        it 'lists all days of the month with 10 as the step' do
-          expect(handler).to eq([1, 11, 21, 31])
-        end
-      end
-
-      describe 'when string is `7-10`' do
-        let(:string) { '7-10' }
-
-        it 'lists all days of the month from 7 to 10' do
-          expect(handler).to eq([7, 8, 9, 10])
-        end
-      end
-
-      describe 'when string is `10,15,16`' do
-        let(:string) { '10,15,16' }
-
-        it 'lists all days of the month passed to the method' do
-          expect(handler).to eq([10, 15, 16])
-        end
-      end
+      let(:minimum) { 1 }
+      let(:maximum) { 31 }
     end
+  end
 
-    context 'with month' do
+  context 'with month' do
+    it_behaves_like 'regexes' do
       let(:type) { :month }
-
-      describe 'when string is `*`' do
-        let(:string) { '*' }
-
-        it 'lists all month from 1 to 12' do
-          expect(handler).to eq((1..12).to_a)
-        end
-      end
-
-      describe 'when string is `*/4`' do
-        let(:string) { '*/4' }
-
-        it 'lists all months with 4 as the step' do
-          expect(handler).to eq([1, 5, 9])
-        end
-      end
-
-      describe 'when string is `7-10`' do
-        let(:string) { '7-10' }
-
-        it 'lists months from 7 to 10' do
-          expect(handler).to eq([7, 8, 9, 10])
-        end
-      end
-
-      describe 'when string is `6,8,10`' do
-        let(:string) { '6,8,10' }
-
-        it 'lists all days of the month passed to the method' do
-          expect(handler).to eq([6, 8, 10])
-        end
-      end
+      let(:minimum) { 1 }
+      let(:maximum) { 12 }
     end
+  end
 
-    context 'with day of the week' do
+  context 'with day of the week' do
+    it_behaves_like 'regexes' do
       let(:type) { :day_of_the_week }
-
-      describe 'when string is `*`' do
-        let(:string) { '*' }
-
-        it 'lists all days of the week from 1 to 7' do
-          expect(handler).to eq((1..7).to_a)
-        end
-      end
-
-      describe 'when string is `*/2`' do
-        let(:string) { '*/2' }
-
-        it 'lists all days of the week with 2 as the step' do
-          expect(handler).to eq([1, 3, 5, 7])
-        end
-      end
-
-      describe 'when string is `4-7`' do
-        let(:string) { '4-7' }
-
-        it 'lists months from 4 to 7' do
-          expect(handler).to eq([4, 5, 6, 7])
-        end
-      end
-
-      describe 'when string is `3,5,7`' do
-        let(:string) { '3,5,7' }
-
-        it 'lists all days of the month passed to the method' do
-          expect(handler).to eq([3, 5, 7])
-        end
-      end
+      let(:minimum) { 1 }
+      let(:maximum) { 7 }
     end
   end
 end
